18d572378354ebe03c6ea489604fbcda85c5d919
trizcard <beatrizco@usp.br>
2022-12-14 17:08:07 -0300

    dividindo func

--- genius.asm
+++ genius.asm
@@ -37,11 +37,59 @@ bloco: #125 ; caracter usado para printar o bloco
 
 jmp main
 
+; da pra dividir em tres grupos
+; um -> piscarSequencia e verificaExec
+; dois-> geradordeAleatorio e escreve main
+; tres -> geraPaginaJogo
 
-main:
+main:   ; gera pagina inicial
+
+
+verificaExec:   ; verifica se a execucao da sequencia foi feita correntamente e aumenta a pontuacao
+
+piscarSequencia:    ; "pisca" os retangulos de acordo com a sequencia
+
+geradordeAleatorio: ;gera um numero entre 0 e 3 para saber qual irá piscar
+                    ; empilha a valor na sequencia
+
+geraPaginaJogo  ; usa a funcao desenharBloco para criar a pagina
+
+desenharBloco:  ; desenho do bloco com centro em x
+                ; r0 posicao de inicio (x - 124)
+                ; r1 = 8 (tamanho da linha)
+                ; r2 cor do bloco
+                ; usar Imprimestr para implentar isso
+
+Imprimestr:		;  Rotina de Impresao de Mensagens:    
+				; r0 = Posicao da tela que o primeiro caractere da mensagem sera' impresso
+				; r1 = endereco onde comeca a mensagem
+				; r2 = cor da mensagem
+				; Obs: a mensagem sera' impressa ate' encontrar "/0"
+				
+;---- Empilhamento: protege os registradores utilizados na subrotina na pilha para preservar seu valor				
+	push r0	; Posicao da tela que o primeiro caractere da mensagem sera' impresso
+	push r1	; endereco onde comeca a mensagem
+	push r2	; cor da mensagem
+	push r3	; Criterio de parada
+	push r4	; Recebe o codigo do caractere da Mensagem
 	
+	loadn r3, #'\0'	; Criterio de parada
 
-
-
-desenharBloco:
-    
+ImprimestrLoop:	
+	loadi r4, r1		; aponta para a memoria no endereco r1 e busca seu conteudo em r4
+	cmp r4, r3			; compara o codigo do caractere buscado com o criterio de parada
+	jeq ImprimestrSai	; goto Final da rotina
+	add r4, r2, r4		; soma a cor (r2) no codigo do caractere em r4
+	outchar r4, r0		; imprime o caractere cujo codigo está em r4 na posicao r0 da tela
+	inc r0				; incrementa a posicao que o proximo caractere sera' escrito na tela
+	inc r1				; incrementa o ponteiro para a mensagem na memoria
+	jmp ImprimestrLoop	; goto Loop
+	
+ImprimestrSai:	
+;---- Desempilhamento: resgata os valores dos registradores utilizados na Subrotina da Pilha
+	pop r4	
+	pop r3
+	pop r2
+	pop r1
+	pop r0
+	rts		; retorno da subrotina
